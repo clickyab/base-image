378ace56a685858124db5b72bbc51d5c3e472e81
fzerorubigd <fzero@rubi.gd>
2016-12-03 22:41:21 +0330

    fix the elasticsearch installation

--- image/helium.sh
+++ image/helium.sh
@@ -3,16 +3,18 @@ set -euo pipefail
 source /bd_build/buildconfig
 
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
+curl https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" >/etc/apt/sources.list.d/elastic-5.x.list
+
 $minimal_apt_get_install wget curl sudo git zsh nano libsqlite3-dev autoconf bison build-essential libssl-dev \
                 libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server mariadb-server mariadb-client mercurial \
                 ruby-dev rabbitmq-server realpath pkg-config unzip dnsutils re2c python-pip htop nodejs \
-                python-dev libpq-dev tmux bzr libsodium-dev cmake default-jdk golang golang-race-detector-runtime python-setuptools
+                python-dev libpq-dev tmux bzr libsodium-dev cmake default-jdk golang golang-race-detector-runtime python-setuptools \
+		elasticsearch
 
 GOBIN=/usr/local/bin GOPATH=/tmp go get -v -u github.com/mailhog/MailHog
 pip install --upgrade --no-cache-dir pip
-cd /tmp && wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-5.0.2.deb
-dpkg -i /tmp/elasticsearch-5.0.2.deb
-rm -f /tmp/elasticsearch-5.0.2.deb
+
 # Create vagrant user
 bash -c "echo root:bita123 | chpasswd"
 groupadd develop
