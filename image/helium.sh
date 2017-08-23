4dbc2cf04dac80e16ce16bd44d3576afa0d83924
fzerorubigd <fzerorubigd@users.noreply.github.com>
2017-08-23 10:41:24 +0430

    Update helium.sh

--- image/helium.sh
+++ image/helium.sh
@@ -7,8 +7,6 @@ cd /tmp
 curl https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz | gunzip | tar xvf -
 /tmp/godeb install 1.8.1
 add-apt-repository ppa:webupd8team/java
-echo "deb http://www.apache.org/dist/cassandra/debian 310x main" > /etc/apt/sources.list.d/cassandra.sources.list
-apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
 
 curl -sL https://deb.nodesource.com/setup_6.x | bash -
 curl https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
@@ -17,19 +15,16 @@ echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selec
 echo debconf shared/accepted-oracle-license-v1-1 seen true  debconf-set-selections
 
 $minimal_apt_get_install wget curl sudo git zsh nano libsqlite3-dev autoconf bison build-essential libssl-dev \
-                libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server mariadb-server mariadb-client mercurial \
+                libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server mariadb-server \
+				mariadb-client mercurial \
                 ruby-dev rabbitmq-server realpath pkg-config unzip dnsutils re2c python-pip htop nodejs \
                 python-dev libpq-dev tmux bzr libsodium-dev cmake oracle-java8-set-default python-setuptools \
-        	    zip postgresql elasticsearch libevent1-dev libconfig-dev liblua5.1-0-dev lua5.1 libjansson-dev cassandra \
-                iputils-ping iproute2 zookeeperd
-
-wget -O /tmp/kafka.tgz http://apache.claz.org/kafka/0.10.2.0/kafka_2.12-0.10.2.0.tgz
-mkdir /opt/Kafka
-tar -xvf /tmp/kafka.tgz -C /opt/Kafka/
+        	    zip elasticsearch libevent1-dev libconfig-dev liblua5.1-0-dev lua5.1 libjansson-dev \
+                iputils-ping iproute2 
 
 GOBIN=/usr/local/bin GOPATH=/tmp go get -v -u github.com/mailhog/MailHog
 pip install --upgrade --no-cache-dir pip
-pip install pgcli
+pip install mycli
 
 # Create vagrant user
 bash -c "echo root:bita123 | chpasswd"
