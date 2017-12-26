2a3f38986ed2156c385f38c08c9e4a177617796a
fzerorubigd <fzerorubigd@gmail.com>
2017-12-26 23:38:53 +0330

    update to 1.9.2

--- image/helium.sh
+++ image/helium.sh
@@ -5,26 +5,24 @@ source /bd_build/buildconfig
 
 cd /tmp
 curl https://godeb.s3.amazonaws.com/godeb-amd64.tar.gz | gunzip | tar xvf -
-/tmp/godeb install 1.8.1
+/tmp/godeb install 1.9.2
 add-apt-repository ppa:webupd8team/java
 
-curl -sL https://deb.nodesource.com/setup_6.x | bash -
+curl -sL https://deb.nodesource.com/setup_9.x | bash -
 curl https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" >/etc/apt/sources.list.d/elastic-5.x.list
 echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true  debconf-set-selections
 
 $minimal_apt_get_install wget curl sudo git zsh nano libsqlite3-dev autoconf bison build-essential libssl-dev \
-                libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server mariadb-server \
-				mariadb-client mercurial \
+                libyaml-dev libreadline6 libreadline6-dev zlib1g zlib1g-dev htop redis-server mariadb-server mariadb-client mercurial \
                 ruby-dev rabbitmq-server realpath pkg-config unzip dnsutils re2c python-pip htop nodejs \
-                python-dev libpq-dev tmux bzr libsodium-dev cmake oracle-java8-set-default python-setuptools \
-        	    zip elasticsearch libevent1-dev libconfig-dev liblua5.1-0-dev lua5.1 libjansson-dev \
-                iputils-ping iproute2 
+                python-dev libpq-dev tmux bzr libsodium-dev cmake oracle-java8-set-default python-setuptools zip \
+        	postgresql elasticsearch libevent1-dev libconfig-dev liblua5.1-0-dev lua5.1 libjansson-dev cassandra iputils-ping iproute2
 
 GOBIN=/usr/local/bin GOPATH=/tmp go get -v -u github.com/mailhog/MailHog
 pip install --upgrade --no-cache-dir pip
-pip install mycli
+pip install pgcli mycli
 
 # Create vagrant user
 bash -c "echo root:bita123 | chpasswd"
